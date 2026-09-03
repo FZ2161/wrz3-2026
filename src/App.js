48a7cbe8c0297bd9903efb5f2e93b5a6b924236a
FZ2161 <filip.zalewski@zs1mm.local>
2026-09-03 16:31:45 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -5,34 +5,48 @@ export default function App() {
 
   const [users, setUsers] = useState([]);
 
+
   useEffect(() => {
     async function getUsers() {
       const response = await fetch(
         "https://jsonplaceholder.typicode.com/users"
       );
-
       const users = await response.json();
       setUsers(users);
-//
-      users.forEach((user) => {
-        console.log(user.name);
-      });
- 
-    }
+      console.log(users)
 
+
+    }
     getUsers();
   }, []);
+
+
+  const deleteUser = (idx) => {
+    setUsers(users.filter((value, index) => index !== idx));
+  };
+
+
+  const addUser = (newName) => {
+    setUsers(
+      [...users, { name: newName }]
+    )
+  }
+  addUser("ddd")
+
+
+
+
 
   return (
     <div>
       {
-       users.map((user, idx) => {
-        return (
-          <div key={idx}>
-            <NameDivs name={user.name} />
-          </div>
-        )
-       }) 
+        users.map((user, idx) => {
+          return (
+            <div key={idx}>
+              <NameDivs user={user} onDelete={() => deleteUser(idx)} />
+            </div>
+          )
+        })
       }
     </div>
   );
--- src/components/names.js
+++ src/components/names.js
@@ -1,10 +1,40 @@
 import React, { useEffect } from "react";
 
-export default function NameDivs({ name }) {
+export default function NameDivs({ user, onDelete }) {
+    return (
 
-  return (
-    <div>
-      <h1>{name}</h1>
-    </div>
-  );
+
+        <div
+            style={{
+                display: "flex",
+                alignItems: "center",
+                gap: "10px",
+                margin: "5px",
+            }}
+        >
+            {/* <div>
+            <input type="text">test</input>
+        </div> */}
+
+            <p>{user.name}</p>
+
+            <button
+                onClick={onDelete}
+                style={{
+                    background: "red",
+                    color: "white",
+                    border: "none",
+                    padding: "5px 8px",
+                    borderRadius: "4px",
+                    cursor: "pointer",
+                }}
+            >
+                delete
+            </button>
+
+
+        </div>
+    );
 }
+
+// dodać usuniecie osoby, przekazać kliknięcie do góry. props, funkcja, która aktualizuje stan. stan i funkcję przekazuję jako propsa
